480d94744abcfcc684b4fa9c2070b8384e61d927
pratheesh <yespratheesh@gmail.com>
2024-06-05 18:36:56 +1000

    errors fixed

--- azuresql/main.tf
+++ azuresql/main.tf
@@ -12,44 +12,25 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "azure_group" {
   name     = "nodeproject_group"
-  location = "East US"
+  location = "Australia East"
 }
 
-resource "azurerm_mysql_server" "mysql_server" {
-  name                = "Azure-mysql-server"
+ resource "azurerm_mysql_flexible_server" "mysql_server" {
+  name                = "nodeprojectmysql"
   location            = azurerm_resource_group.azure_group.location
   resource_group_name = azurerm_resource_group.azure_group.name
-  sku_name            = "B_Gen5_1"
-  version             = "5.7"
-  storage_mb          = 5120  # 5 GB
-
+  sku_name               = "GP_Standard_D2ds_v4"
   administrator_login          = var.mysql_admin_username
-  administrator_login_password = var.mysql_admin_password
-
+  administrator_password = var.mysql_admin_password
   tags = {
     environment = "production"
   }
+}
 
-  ssl_enforcement_enabled = true  # Enable SSL enforcement for secure connections
-
-  # Configure backup retention
-  backup {
-    retention_days = 7
-    geo_redundant_backup_enabled = false
-  }
-
-  # Define a firewall rule to allow all Azure services to access the server
-  firewall_rule {
-    name                        = "AllowAllWindowsAzureIps"
-    start_ip_address            = "0.0.0.0"
-    end_ip_address              = "0.0.0.0"
-  }
-
-  # Define a firewall rule to allow specific IP addresses
-  # Replace "x.x.x.x" and "y.y.y.y" with actual IP addresses or ranges
-  firewall_rule {
-    name                        = "AllowFromSpecificIPs"
-    start_ip_address            = "0.0.0.0"
-    end_ip_address              = "0.0.0.0"
-  }
+resource "azurerm_mysql_flexible_server_firewall_rule" "allow_eks_node_1" {
+  name                = "AllowEKSNode1"
+  resource_group_name = azurerm_resource_group.azure_group.name
+  server_name         = azurerm_mysql_flexible_server.mysql_server.name
+  start_ip_address    = "0.0.0.0"  # Replace with actual IP
+  end_ip_address      = "0.0.0.0"  # Replace with actual IP
 }
